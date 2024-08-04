d21e39302e7cf452fcc0d4e1158d21c995083084
dimzka <dimas9aul2@gmail.com>
2024-08-04 21:40:05 +0700

    change laporan keluhan

--- petugas/data-keluhan.php
+++ petugas/data-keluhan.php
@@ -31,82 +31,88 @@ if (!$result) {
 ?>
 <!-- Main content -->
 <section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
-        <div class="card">
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
 
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">DATA KELUHAN</h3>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
-            <table id="example1" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>ID Keluhan</th>
-                  <th>Nama Pelanggan</th>
-                  <th>Saran</th>
-                  <th>Kritik</th>
-                  <th>Dibuat</th>
-                  <th>Status</th>
-                  <th>Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php while ($row = $result->fetch_assoc()) : ?>
-                  <tr>
-                    <td><?php echo htmlspecialchars($row['id_keluhan']); ?></td>
-                    <td><?php echo htmlspecialchars($row['nama_pelanggan']); ?></td>
-                    <td><?php echo htmlspecialchars($row['saran']); ?></td>
-                    <td><?php echo htmlspecialchars($row['kritik']); ?></td>
-                    <td><?php echo htmlspecialchars($row['created_at']); ?></td>
-                    <td>
-                      <?php
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">DATA KELUHAN</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID Keluhan</th>
+                                    <th>Nama Pelanggan</th>
+                                    <th>Saran</th>
+                                    <th>Kritik</th>
+                                    <th>Dibuat</th>
+                                    <th>Status</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php while ($row = $result->fetch_assoc()) : ?>
+                                <tr>
+                                    <td><?php echo htmlspecialchars($row['id_keluhan']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['nama_pelanggan']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['saran']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['kritik']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['created_at']); ?></td>
+                                    <td>
+                                        <?php
                       if ($row['status'] == 0) {
                         echo "Pending";
                       } else {
                         echo $row['status'];
                       }
                       ?>
-                    </td>
-                    <td>
-                      <?php if ($row['status'] == 0) : ?>
-                        <a href="index.php?page=data-keluhan&action=setuju&id=<?php echo $row['id_keluhan']; ?>" class="btn btn-success" onclick="return confirm('Apakah Anda yakin ingin menyetujui keluhan ini?')">
-                          <i class="fas fa-check"></i>
-                          Setuju
-                        </a>
-                        <a href="index.php?page=data-keluhan&action=tolak&id=<?php echo $row['id_keluhan']; ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin membatalkan keluhan ini?')">
-                          <i class="fas fa-times"></i>
-                          Tolak
-                        </a>
-                      <?php endif; ?>
-                    </td>
-                  </tr>
-                <?php endwhile; ?>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th>ID Keluhan</th>
-                  <th>Nama Pelanggan</th>
-                  <th>Saran</th>
-                  <th>Kritik</th>
-                  <th>Dibuat</th>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-          <!-- /.card-body -->
+                                    </td>
+                                    <td>
+                                        <?php if ($row['status'] == 0) : ?>
+                                        <a href="index.php?page=data-keluhan&action=setuju&id=<?php echo $row['id_keluhan']; ?>"
+                                            class="btn btn-success"
+                                            onclick="return confirm('Apakah Anda yakin ingin menyetujui keluhan ini?')">
+                                            <i class="fas fa-check"></i>
+                                            Setuju
+                                        </a>
+                                        <a href="index.php?page=data-keluhan&action=tolak&id=<?php echo $row['id_keluhan']; ?>"
+                                            class="btn btn-danger"
+                                            onclick="return confirm('Apakah Anda yakin ingin membatalkan keluhan ini?')">
+                                            <i class="fas fa-times"></i>
+                                            Tolak
+                                        </a>
+                                        <?php endif; ?>
+                                    </td>
+                                </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>ID Keluhan</th>
+                                    <th>Nama Pelanggan</th>
+                                    <th>Saran</th>
+                                    <th>Kritik</th>
+                                    <th>Dibuat</th>
+                                    <th>Status</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
         </div>
-        <!-- /.card -->
-      </div>
-      <!-- /.col -->
+        <!-- /.row -->
     </div>
-    <!-- /.row -->
-  </div>
-  <!-- /.container-fluid -->
+    <!-- /.container-fluid -->
 </section>
